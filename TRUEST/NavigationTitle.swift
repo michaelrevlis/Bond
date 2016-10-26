29d0f6fc5b284e8a17ea90109745822a5f650214
Xrescent <xrescent@gmail.com>
2016-10-26 14:47:30 +0800

    1.passcode feature added 2. navi title bug fixed 3.sign color fixed

---
+++ TRUEST/NavigationTitle.swift
@@ -0,0 +1,9 @@
+//
+//  NavigationTitle.swift
+//  TRUEST
+//
+//  Created by 林柏翰 on 2016/10/24.
+//  Copyright © 2016年 MichaelRevlis. All rights reserved.
+//
+
+import Foundation
---
+++ TRUEST/PasscodeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PasscodeViewController.swift
+//  TRUEST
+//
+//  Created by 林柏翰 on 2016/10/25.
+//  Copyright © 2016年 MichaelRevlis. All rights reserved.
+//
+
+import Foundation
